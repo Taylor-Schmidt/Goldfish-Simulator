3c327c54cdc27979928e1922340a93edcc036409
Taylor-Schmidt <47361741+Taylor-Schmidt@users.noreply.github.com>
2021-11-26 21:48:03 -0500

    Added collision on walls

--- src/goldfish.ts
+++ src/goldfish.ts
@@ -18,7 +18,25 @@ export class Goldfish extends ex.Actor{
     private onPreCollision(evt: ex.PreCollisionEvent){
 
     }
-    
+    onPostUpdate(engine: ex.Engine){
+
+        //collide left side
+        if(this.pos.x < this.width /2){
+            this.vel.x *= -1;
+        }
+        //collide right side
+        if(this.pos.y < this.height/2){
+            this.vel.y *= -1;
+        }
+        //collide top
+        if (this.pos.x + this.width / 2 > 800){
+            this.vel.x *= -1;
+        }
+        if (this.pos.y + this.height / 2 > 800){
+            this.vel.y *= -1;
+        }
+
+    }
 
 
 }
