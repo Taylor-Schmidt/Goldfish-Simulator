eec6276f046957514d9a2f2d4ca0dbb492914f83
Taylor-Schmidt <47361741+Taylor-Schmidt@users.noreply.github.com>
2021-11-28 21:54:01 -0500

    fixed issue with goldfish

--- src/goldfish.ts
+++ src/goldfish.ts
@@ -29,6 +29,7 @@ export class Goldfish extends ex.Actor{
         let b = Math.floor(Math.random() * 255);
         fish.colorize(new ex.Color(r,g,b,100));
         this.addDrawing("right", fish );
+        console.log(`anchor: ${this.anchor}`);
         
     }
 
@@ -38,24 +39,11 @@ export class Goldfish extends ex.Actor{
     onPostUpdate(engine: ex.Engine){
 
        
-            this.deciding = Math.floor(Math.random() * 1000)
-            if(this.deciding === 1){
-                console.log("Decided!")
-                console.log(`pos: ${this.pos}`)
-                this.newPos.x = Math.floor(Math.random() * (Config.GameWidth - this.width/2) + (this.width/2)); 
-                this.newPos.y = Math.floor(Math.random() * (Config.GameHeight - this.height/2) + (this.height/2));
-                console.log(`newPossition: ${this.newPos}`);
-                this.halfWay.x = (this.newPos.x + this.startPosition.x)/2;
-                this.halfWay.y = (this.newPos.y + this.startPosition.y)/2;
-                this.xdif=(this.newPos.x - this.startPosition.x);
-                console.log(`xdif: ${this.xdif}`)
-                this.ydif=(this.newPos.y - this.startPosition.y);
-                console.log(`ydif: ${this.ydif}`)
-                console.log(`halfway: ${this.halfWay}`);
-            }
-            this.swim();
-
-
+        this.randomMove();
+        this.checkCollision();
+        this.swim();
+    }
+    private checkCollision(){
         //collide left side
         if(this.pos.x < this.width /2){
             this.vel.x *= -0.9;
@@ -72,11 +60,28 @@ export class Goldfish extends ex.Actor{
         if (this.pos.y + this.height / 2 > Config.GameHeight){
             this.vel.y *= -0.9;
         }
-
     }
 
+    private randomMove(){
+        this.deciding = Math.floor(Math.random() * 1000)
+        if(this.deciding === 1){
+            console.log("Decided!")
+            console.log(`pos: ${this.pos}`)
+            this.newPos.x = Math.floor(Math.random() * (Config.GameWidth - this.width/2) + (this.width/2)); 
+            this.newPos.y = Math.floor(Math.random() * (Config.GameHeight - this.height/2) + (this.height/2));
+            console.log(`newPossition: ${this.newPos}`);
+            this.halfWay.x = (this.newPos.x + this.startPosition.x)/2;
+            this.halfWay.y = (this.newPos.y + this.startPosition.y)/2;
+            this.xdif=(this.newPos.x - this.startPosition.x);
+            console.log(`xdif: ${this.xdif}`)
+            this.ydif=(this.newPos.y - this.startPosition.y);
+            console.log(`ydif: ${this.ydif}`)
+            console.log(`halfway: ${this.halfWay}`);
+        }  
+    }
     private swim(){
         console.log("swimming");
+        console.log(`vel: ${this.vel}`);
         if(Math.ceil(this.pos.x) === this.newPos.x && Math.ceil(this.pos.y) === this.newPos.y)
         {
             this.vel = new ex.Vector(0,0);
@@ -85,7 +90,7 @@ export class Goldfish extends ex.Actor{
             return;
         }
          //moving into positive x
-         if(this.xdif < 0)
+         if(this.xdif > 0)
          {
              if(this.pos.x < this.halfWay.x){
                  this.vel.x += 0.1;
@@ -97,14 +102,14 @@ export class Goldfish extends ex.Actor{
          //moving into negative x
          else{           
              if(this.pos.x > this.halfWay.x){
-                 this.vel.x += 0.1;
+                 this.vel.x -= 0.1;
              }
              else{
-                 this.vel.x -= 0.1;
+                 this.vel.x += 0.1;
              }
          }
          //moving into positive y
-         if(this.ydif < 0)
+         if(this.ydif > 0)
          {
              if(this.pos.y < this.halfWay.y){
                  this.vel.y += 0.1;
@@ -116,15 +121,16 @@ export class Goldfish extends ex.Actor{
          //moving into negative y
          else{           
              if(this.pos.y > this.halfWay.y){
-                 this.vel.y += 0.1;
+                 this.vel.y -= 0.1;
              }
              else{
-                 this.vel.y -= 0.1;
+                 this.vel.y += 0.1;
              }
          }
        
         
     }
+
 
 
 }
